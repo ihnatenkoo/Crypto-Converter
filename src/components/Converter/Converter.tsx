d036323ca7fefca3d7b4cc10b27d4841c35070c8
Dmytro <ihnatenko.dev@gmail.com>
2022-08-20 22:01:46 +0300

    Impl converter logic in <Converter/> <ConverterItem/>

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,17 +1,57 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+import currenciesStore from '../../stores/currenciesStore';
+
 import ConverterItem from '../ConverterItem/ConverterItem';
-import converterStore from '../../stores/converterStore';
 
 import s from './Converter.module.scss';
 
 const Converter: FC = observer(() => {
-	const pickedCoin = converterStore.getSelectedCoin;
+	const allCoins = currenciesStore.getCoins;
+
+	const [fromCurrency, setFromCurrency] = useState<string>('');
+	const [toCurrency, setToCurrency] = useState<string>('');
+	const [exchangeRate, setExchangeRate] = useState<number>(0);
+	const [amount, setAmount] = useState<number>(1);
+	const [amountInFromCurrency, setAmountInFromCurrency] =
+		useState<boolean>(true);
+
+	let toAmount, fromAmount;
+	if (amountInFromCurrency) {
+		fromAmount = amount;
+		toAmount = +(amount * exchangeRate).toFixed(10);
+	} else {
+		fromAmount = +(amount / exchangeRate).toFixed(10);
+		toAmount = amount;
+	}
+
+	const handleFromAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setAmount(+e.target.value);
+		setAmountInFromCurrency(true);
+	};
+
+	const handleToAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setAmount(+e.target.value);
+		setAmountInFromCurrency(false);
+	};
+
+	const getExchangeRate = () => {
+		const priceFrom = allCoins.find(
+			(coin) => coin.name === fromCurrency
+		)?.price;
+		const priceTo = allCoins.find((coin) => coin.name === toCurrency)?.price;
+
+		if (priceFrom && priceTo) setExchangeRate(priceFrom / priceTo);
+	};
+
+	useEffect(() => {
+		getExchangeRate();
+	}, [fromCurrency, toCurrency]);
 
 	return (
 		<Paper elevation={2}>
@@ -21,11 +61,22 @@ const Converter: FC = observer(() => {
 				autoComplete="off"
 				className={s.converter}
 			>
-				<ConverterItem pickedCoin={pickedCoin} />
-				<ConverterItem />
+				<ConverterItem
+					currency={fromCurrency}
+					onChangeCurrency={setFromCurrency}
+					value={fromAmount}
+					onChangeValue={handleFromAmountChange}
+				/>
+				<ConverterItem
+					currency={toCurrency}
+					onChangeCurrency={setToCurrency}
+					value={toAmount}
+					onChangeValue={handleToAmountChange}
+				/>
 				<Typography variant="h6">Total:</Typography>
 			</Box>
 		</Paper>
 	);
 });
+
 export default Converter;
--- src/components/ConverterItem/ConverterItem.tsx
+++ src/components/ConverterItem/ConverterItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { ChangeEvent, Dispatch, FC, SetStateAction } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import TextField from '@mui/material/TextField';
@@ -8,46 +8,52 @@ import currenciesStore from '../../stores/currenciesStore';
 
 import s from './ConverterItem.module.scss';
 
-interface a {
-	pickedCoin?: string;
+interface IConverterItem {
+	currency: string;
+	value: number;
+	onChangeCurrency: Dispatch<SetStateAction<string>>;
+	onChangeValue: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const ConverterItem: FC<a> = observer(({ pickedCoin }) => {
-	const [selectedCoin, setSelectedCoin] = useState('');
-	const allCoins = currenciesStore.getCoins;
+const ConverterItem: FC<IConverterItem> = observer(
+	({ currency, onChangeCurrency, value, onChangeValue }) => {
+		const allCoins = currenciesStore.getCoins;
 
-	useEffect(() => {
-		if (pickedCoin) setSelectedCoin(pickedCoin);
-	}, [pickedCoin]);
+		const changeCurrencyHandler = (e: ChangeEvent<HTMLInputElement>) => {
+			onChangeCurrency(e.target.value);
+		};
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSelectedCoin(event.target.value);
-	};
+		return (
+			<div className={s.converter__item}>
+				<TextField
+					id="outlined-basic"
+					label="Value"
+					variant="outlined"
+					value={value}
+					onChange={onChangeValue}
+				/>
 
-	return (
-		<div className={s.converter__item}>
-			<TextField id="outlined-basic" label="Value" variant="outlined" />
-
-			<TextField
-				id="outlined-select-currency"
-				select
-				label="Currency"
-				value={selectedCoin}
-				onChange={handleChange}
-			>
-				{allCoins.map((coin) => (
-					<MenuItem value={coin.name} key={coin.fullName}>
-						<img
-							src={coin.imageUrl}
-							alt="coin icon"
-							className={s.converter__img}
-						/>
-						{coin.name}
-					</MenuItem>
-				))}
-			</TextField>
-		</div>
-	);
-});
+				<TextField
+					id="outlined-select-currency"
+					select
+					label="Currency"
+					value={currency}
+					onChange={changeCurrencyHandler}
+				>
+					{allCoins.map((coin) => (
+						<MenuItem value={coin.name} key={coin.fullName}>
+							<img
+								src={coin.imageUrl}
+								alt="coin icon"
+								className={s.converter__img}
+							/>
+							{coin.name}
+						</MenuItem>
+					))}
+				</TextField>
+			</div>
+		);
+	}
+);
 
 export default ConverterItem;
